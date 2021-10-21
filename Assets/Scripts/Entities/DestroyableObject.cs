d5138cdc1c8820b3a39fe70dcb989b99e84c440b
NicolasIceberg <user001ggog@gmail.com>
2021-10-21 23:02:37 +0300

    Add player hit box, WIP box and barrel

---
+++ Assets/Scripts/Entities/DestroyableObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities
+{
+    public class DestroyableObject : Entity
+    {
+        protected override void OnDie()
+        {
+        }
+
+        protected override void OnHeal()
+        {
+        }
+
+        protected override void OnHurt()
+        {
+        }
+
+        protected override void OnMove()
+        {
+        }
+
+        protected override void OnStartMoving()
+        {
+        }
+
+        protected override void OnStopMoving()
+        {
+        }
+    }
+}
